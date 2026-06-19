0dacbcbe20456ea95f05a5055d5ac0cac1b046e3
Gilmar Rodrigues Campelo <106250978+Gilmar1987@users.noreply.github.com>
2026-06-19 15:27:58 -0300

    Enable password validation regex checks

--- src/schemas/User.Schema.ts
+++ src/schemas/User.Schema.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 const passwordValidation = z.string()
   .min(6, 'A senha deve conter no mínimo 6 caracteres')
-  //.regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
-  //.regex(/[0-9]/, 'A senha deve conter pelo menos um número')
-  //.regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial');
+  .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
+  .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
+  .regex(/[^a-zA-Z0-9]/, 'A senha deve conter pelo menos um caractere especial');
 
 export const UserSchema = z.object({
   name: z.string().min(1, 'O nome é obrigatório'),
